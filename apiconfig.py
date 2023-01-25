06000f9a97a03b846a1b64036781ae4788d580d8
Dmitriy Belkin <demetrius.belkin@gmail.com>
2023-01-25 17:18:43 +0300

    New

---
+++ apiconfig.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+
+# class ApiConfig():
+
+
+API_TOKEN = 'TOKEN'
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+vex = Dispatcher(bot)
+    
+
+@vex.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm Vex!\nPowered by Dmitriy Belkin.")
+    
+@vex.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(vex, skip_updates=True)
